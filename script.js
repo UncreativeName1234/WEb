d49669662924b25256279b9fcc9fcdc04352f051
UncreativeName1234 <73205828+UncreativeName1234@users.noreply.github.com>
2020-12-22 20:19:26 -0500

    Nav basically

--- script.js
+++ script.js
@@ -47,3 +47,45 @@ if (window.addEventListener) {
 }   else if (window.attachEvent) {
     window.attachEvent("onclick", createEventListener);
 }
+
+function getRidOfStuff() {
+    var rating = document.getElementById("rating");
+    var comments = document.getElementById("comments");
+
+    rating.selectedIndex = 0;
+    $( 'input[type="checkbox"]' ).prop('checked', false);
+    comments.innerHTML = "";
+}
+
+function array() {
+    var design = document.getElementById("design");
+    var content = document.getElementById("content");
+    var bugs = document.getElementById("bugs");
+    var output1 = document.getElementById("out1");
+    var feedback = [];
+    
+    if (design.checked === true) {
+        feedback.push("Design");
+    } if (content.checked === true) {
+        feedback.push(" Content");
+    } if (bugs.checked === true) {
+        feedback.push(" Bugs");
+    }
+    
+    output1.innerHTML = feedback.toString();
+}
+
+if (window.addEventListener) {
+    window.addEventListener("click", array, false);
+}   else if (window.attachEvent) {
+    window.attachEvent("onclick", array);
+}
+
+function hamburg() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
